c86978ddfe1255ee6cd3cd74b02dcc8ff38d5c10
Renan Rey <renan.reyc@gmail.com>
2022-05-25 22:36:31 -0300

    ajuste AnimeService e package requeste

--- src/main/java/academy/devdojo/springboot2/domain/Anime.java
+++ src/main/java/academy/devdojo/springboot2/domain/Anime.java
@@ -1,6 +1,7 @@
 package academy.devdojo.springboot2.domain;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -10,6 +11,7 @@ import javax.persistence.*;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
+@Builder
 @Table(name = "anime")
 public class Anime {
 
--- src/main/java/academy/devdojo/springboot2/service/AnimeService.java
+++ src/main/java/academy/devdojo/springboot2/service/AnimeService.java
@@ -1,48 +1,47 @@
-//package academy.devdojo.springboot2.service;
-//
-//import academy.devdojo.springboot2.domain.Anime;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.stereotype.Service;
-//import org.springframework.web.server.ResponseStatusException;
-//
-//import java.util.ArrayList;
-//import java.util.List;
-//import java.util.concurrent.ThreadLocalRandom;
-//
-//@Service
-//public class AnimeService {
-//    private static List<Anime> animes;
-//
-//    static {
-//        animes = new ArrayList<>(List.of(new Anime(1L, "DBZ"), new Anime(2L, "Berserk")));
-//    }
-//
-//
-//    // private final AnimeRepository aniumeRepository
-//    public List<Anime> listAll() {
-//        return animes;
-//    }
-//
-//    public Anime findById(long id) {
-//
-//        return animes.stream()
-//                .filter(anime -> anime.getId().equals(id))
-//                .findFirst()
-//                .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Anime not Found."));
-//    }
-//
-//    public Anime save(Anime anime) {
-//        anime.setId(ThreadLocalRandom.current().nextLong(3, 100000));
-//        animes.add(anime);
-//        return anime;
-//    }
-//
-//    public void delete(long id) {
-//        animes.remove(findById(id));
-//    }
-//
-//    public void replace(Anime anime) {
-//        delete(anime.getId());
-//        animes.add(anime);
-//    }
-//}
+package academy.devdojo.springboot2.service;
+
+import academy.devdojo.springboot2.domain.Anime;
+import academy.devdojo.springboot2.repository.AnimeRepository;
+import academy.devdojo.springboot2.requests.AnimePostRequestBody;
+import academy.devdojo.springboot2.requests.AnimePutRequestBody;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+
+
+@Service
+@RequiredArgsConstructor
+public class AnimeService {
+    private final AnimeRepository animeRepository;
+
+    public List<Anime> listAll() {
+        return animeRepository.findAll();
+    }
+
+    public Anime findByIdOrThrowBadRequestException(long id) {
+        return animeRepository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Anime not Found."));
+    }
+
+    public Anime save(AnimePostRequestBody animePostRequestBody) {
+        Anime anime = Anime.builder().name(animePostRequestBody.getName()).build();
+        return animeRepository.save(anime);
+    }
+
+    public void delete(long id) {
+        animeRepository.delete((findByIdOrThrowBadRequestException(id)));
+    }
+
+    public void replace(AnimePutRequestBody animePutRequestBody) {
+        Anime savedAnime = findByIdOrThrowBadRequestException(animePutRequestBody.getId());
+        Anime anime = Anime.builder()
+                .id(savedAnime.getId())
+                .name(animePutRequestBody.getName())
+                .build();
+
+        animeRepository.save(anime);
+    }
+}
